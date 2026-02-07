d5fcbe413354cf63f7a8431e9448938491c9eb0f
bloodymirhernandez-cyber <233523080+bloodymirhernandez-cyber@users.noreply.github.com>
2026-02-07 13:43:21 +0800

    car
    car

---
+++ fxmanifest.lua
@@ -0,0 +1,156 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+files {
+    'data/**/*.meta',
+    'data/**/*.meta',
+    'data/**/*.xml',
+    'data/**/*.dat',
+    'data/**/*.ytyp',
+
+    'audioconfig/*.dat151.rel',
+    'audioconfig/*.dat54.rel',
+    'sfx/**/*.awc'
+}
+
+data_file 'HANDLING_FILE'                     'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'              'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE'             'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE'                      'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'            'data/**/carvariations.meta'
+data_file 'DLCTEXT_FILE'                      'data/**/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE'            'data/**/carcontentunlocks.meta'
+
+-- [EXCLUSIVE CARS] --
+-- m8f91 Jek --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+
+
+-- Sunrise --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sunrise_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sunrise_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sunrise'
+
+-- Limited EK9 --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/k20a_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/k20a_sounds.dat' 
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_k20a'
+
+-- hymn_eg | hymn_fdfeels --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq21honb16a_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq21honb16a_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq21honb16a'
+
+-- radfortuner20 Isabelle--
+data_file 'AUDIO_GAMEDATA' 'audioconfig/brabus850_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/brabus850_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brabus850'
+
+-- r150r Dawn --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/chevroletlt4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/chevroletlt4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_chevroletlt4'
+
+-- rmodsuprapandem Dawn --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/perfov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/perfov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_perfov10'
+
+-- 1500ghoul Beach --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_amp.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+
+-- hilux21 Jek --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg125mnsrybently_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg125mnsrybently_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg125mnsrybently'
+
+-- cbr1000rrr Jek --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/panigalevtwinsound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/panigalevtwinsound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_panigalevtwinsound'
+
+-- [m4gt3] [Ben Smith] --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg18bmwm4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg18bmwm4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg18bmwm4'
+
+-- est_sohc_turbo --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f40v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
+
+-- honintdc2 --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq22honb18c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq22honb18c_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq22honb18c'
+
+-- LaQuica_SiRbody --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq32hond16z6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq32hond16z6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq32hond16z6'
+
+-- LaQuica_SIRdrag --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta062k20a_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta062k20a_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta062k20a'
+
+-- nisgtrr32 --
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tarb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tarb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tarb26dett'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/kc29hondak24z7_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/kc29hondak24z7_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_kc29hondak24z7'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/488sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/488sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_488sound'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/kc1936knuckle_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/kc1936knuckle_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_kc1936knuckle'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta4b11_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta4b11_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta4b11'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/thrustp_game.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nsck24ttotoy_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/nsck24ttotoy_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nsck24ttotoy'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gallardov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gallardov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gallardov10'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/fordvoodoo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/fordvoodoo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fordvoodoo'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq11bmw298cc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq11bmw298cc_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq11bmw298cc'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta103ninjah2r_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta103ninjah2r_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta103ninjah2r'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/r35sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vr38dettv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vr38dettv6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vr38dettv6'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg81hcredeye_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg81hcredeye_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg81hcredeye'
